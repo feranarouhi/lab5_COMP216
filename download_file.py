5de971651ed306637a81cb9ea21a51df9c076f77
ferana rouhi <124641424+feranarouhi@users.noreply.github.com>
2024-04-01 14:01:37 -0400

    Add files via upload

---
+++ download_file.py
@@ -0,0 +1,15 @@
+
+
+import requests
+
+pathname = 'client_download/text.txt'
+requested_file = 'test.txt'
+URL = f'http://127.0.0.1:5000/download/{requested_file}'           #request all users
+
+r = requests.get(URL, stream=True)               # make a HTTP GET request
+
+with open(pathname, 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=50):  #ridiculously small size
+        print('+', end='')                       #give ser feedback
+        fd.write(chunk)                          #write the bit to file
+    print('\n...download completed')             #complete
